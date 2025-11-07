60600ccdab57c01a785281b6aba7267cc7aaff48
Jonbosss <jonathanfarhat@gmail.com>
2025-11-07 12:50:22 -0800

    Finished list exercise; Created list of the weekdays which are selectable and viewable with a button

--- dropdown.py
+++ dropdown.py
@@ -5,11 +5,29 @@ root = tk.Tk()
 root.title('Drop Boxes')
 root.geometry('400x400')
 
-clicked = StringVar()
 
-drop = OptionMenu(root, clicked, "Monday", "Tuesday",
-                  "Wednesday", "Thursday", "Friday")
+def show():
+    myLabel = Label(root, text=clicked.get()).pack()
+
+
+options = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday"
+
+]
+
+clicked = StringVar()
+clicked.set(options[0])
+
+drop = OptionMenu(root, clicked, *options)
 drop.pack()
+
+myButton = Button(root, text="Show Selection", command=show).pack()
 
 
 root.mainloop()
